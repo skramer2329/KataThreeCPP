fc965c60945e3633eb692b0cd9bc03cbc0477c36
skramer2329 <skramer2329@gmail.com>
2021-03-13 21:11:49 -0600

    reviewing basic datatypes and constants

--- KataThreeCPP/main.cpp
+++ KataThreeCPP/main.cpp
@@ -27,6 +27,27 @@ int main() {
 	double power = pow(base, exponent);
 	std::cout << power << std::endl;
 
+	//bool datatypes:
+	bool found = false;
+	//search algorithm -> if found, found = true;
+	//std::cout << found << std::endl; will only print found as 0 to the console. std::boolalpha will print found as false to the console.
+	std::cout << std::boolalpha << found << std::endl;
+
+	//This is another way to print...
+	found = true;
+	if (found) std::cout << "This is true";
+
+	//floating point numbers:
+
+	double b = 7.7E4; //This is one way to store a double, 77000
+	std::cout << b << std::endl;
+
+	float a = 10.0 / 3; //float is 6 precise digits. Double is 15 precise digits. Long double is 18 precise digits. Best to go with doubles for the most part...
+
+	//Constants:
+	const int x = 5; //read only variable
+	enum { y = 100 }; //another constant... not sure what difference is.
+
 	return 0;
 }
 
